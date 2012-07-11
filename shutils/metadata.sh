e9b0173df9a5c36621b22412adc6183cbdd78325
Juan RP <xtraeme@gmail.com>
2012-07-11 11:10:40 +0200

    metadata: change "build_date" object to `LANG=C date "+%x %X %Z"`.

--- shutils/metadata.sh
+++ shutils/metadata.sh
@@ -371,7 +371,7 @@ _EOF
 
 	# Build date.
 	echo "<key>build_date</key>" >> $TMPFPROPS
-	echo "<string>$(LANG=C date -u "+%A %d %B, %Y, %T UTC")</string>" >> $TMPFPROPS
+	echo "<string>$(LANG=C date "+%x %X %Z")</string>" >> $TMPFPROPS
 
 	# Homepage
 	if [ -n "$homepage" ]; then
