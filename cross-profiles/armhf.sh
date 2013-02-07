1422da52a141dbbe3ec551dfa9fc6d90786ceee6
Juan RP <xtraeme@gmail.com>
2013-02-07 16:07:22 +0100

    cross-profiles/armhf.sh: override XBPS_C{,XX}FLAGS.

--- cross-profiles/armhf.sh
+++ cross-profiles/armhf.sh
@@ -2,5 +2,7 @@
 
 XBPS_TARGET_ARCH="armv6l"
 XBPS_CROSS_TRIPLET="arm-linux-gnueabihf"
-XBPS_CROSS_CFLAGS="-O2 -pipe -march=armv6 -mfpu=vfp -mfloat-abi=hard"
+XBPS_CFLAGS="-O2 -pipe"
+XBPS_CXXFLAGS="$XBPS_CFLAGS"
+XBPS_CROSS_CFLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=hard"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
