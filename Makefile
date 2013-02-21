4de5fe3132ceacd4aa1cc5f306d1db247341d435
Juan RP <xtraeme@gmail.com>
2013-02-21 19:07:37 +0100

    Added support to cross build python pkgs (build_style=python-module).

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ ETCDIR  ?= $(PREFIX)/etc/xbps
 PRIVILEGED_GROUP ?= wheel
 
 # INMUTABLE VARIABLES
-VERSION	= 38
+VERSION	= 39
 GITVER	:= $(shell git rev-parse HEAD)
 CONF_FILE = xbps-src.conf
 
--- helpers/python-module.sh
+++ helpers/python-module.sh
@@ -1,13 +1,39 @@
 #
 # This helper is for templates installing python modules.
 #
+XBPS_PYVER="2.7" # currently 2.7 is the default python
+
 do_build() {
-	python setup.py build ${make_build_args}
+	if [ -n "$XBPS_CROSS_TRIPLET" ]; then
+		CC="${XBPS_CROSS_TRIPLET}-gcc -pthread"
+		LDSHARED="${CC} -shared"
+		CROSSBASE="/usr/$XBPS_CROSS_TRIPLET"
+		PYPREFIX="$CROSSBASE"
+		CFLAGS="$CFLAGS -I${CROSSBASE}/include/python${XBPS_PYVER} -I${CROSSBASE}/usr/include"
+		LDFLAGS="$LDFLAGS -L${CROSSBASE}/lib/python${XBPS_PYVER} -L${CROSSBASE}/lib"
+		env CC="$CC" LDSHARED="$LDSHARED" \
+			PYPREFIX="$PYPREFIX" CFLAGS="$CFLAGS" \
+			LDFLAGS="$LDFLAGS" python setup.py build ${make_build_args}
+	else
+		python setup.py build ${make_build_args}
+	fi
 }
 
 do_install() {
 	if [ -z "$make_install_args" ]; then
 		make_install_args="--prefix=/usr --root=$DESTDIR"
 	fi
-	python setup.py install ${make_install_args}
+	if [ -n "$XBPS_CROSS_TRIPLET" ]; then
+		CC="${XBPS_CROSS_TRIPLET}-gcc -pthread"
+		LDSHARED="${CC} -shared"
+		CROSSBASE="/usr/$XBPS_CROSS_TRIPLET"
+		PYPREFIX="$CROSSBASE"
+		CFLAGS="$CFLAGS -I${CROSSBASE}/include/python${XBPS_PYVER} -I${CROSSBASE}/usr/include"
+		LDFLAGS="$LDFLAGS -L${CROSSBASE}/lib/python${XBPS_PYVER} -L${CROSSBASE}/lib"
+		env CC="$CC" LDSHARED="$LDSHARED" \
+			PYPREFIX="$PYPREFIX" CFLAGS="$CFLAGS" \
+			LDFLAGS="$LDFLAGS" python setup.py install ${make_install_args}
+	else
+		python setup.py install ${make_install_args}
+	fi
 }
