58309edd6bd4c22386a85e2d37910ef89278b9d6
Juan RP <xtraeme@gmail.com>
2013-06-16 07:14:30 +0200

    helpers/cmake.sh: make sure that cmake runs in cwd.

--- helpers/cmake.sh
+++ helpers/cmake.sh
@@ -2,9 +2,10 @@
 # This helper is for templates using cmake.
 #
 do_configure() {
-	[ -z "$configure_script" ] && configure_script=cmake
-	${configure_script} -DCMAKE_INSTALL_PREFIX=/usr \
-		-DCMAKE_BUILD_TYPE=Release ${configure_args}
+	cmake -DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_SKIP_RPATH=ON \
+		${configure_args} .
 }
 
 # cmake scripts use make(1) to build/install.
