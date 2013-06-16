c3f44c1edc0220d14dc681699f03e7e1ab898a45
Juan RP <xtraeme@gmail.com>
2013-06-16 08:07:56 +0200

    helpers/cmake.sh: use out of tree builds, required by kde.

--- helpers/cmake.sh
+++ helpers/cmake.sh
@@ -2,11 +2,33 @@
 # This helper is for templates using cmake.
 #
 do_configure() {
+	[ ! -d build ] && mkdir build
+	cd build
 	cmake -DCMAKE_INSTALL_PREFIX=/usr \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DCMAKE_SKIP_RPATH=ON \
-		${configure_args} .
+		${configure_args} ..
 }
 
-# cmake scripts use make(1) to build/install.
-. $XBPS_HELPERSDIR/gnu-makefile.sh
+do_build() {
+	if [ -z "$make_cmd" ]; then
+		make_cmd=make
+	fi
+	cd build
+	${make_cmd} ${makejobs} ${make_build_args} ${make_build_target}
+}
+
+do_install() {
+	local target
+
+	if [ -z "$make_install_target" ]; then
+		target="DESTDIR=${DESTDIR} install"
+	else
+		target="${make_install_target}"
+	fi
+	if [ -z "$make_cmd" ]; then
+		make_cmd=make
+	fi
+	cd build
+	${make_cmd} ${make_install_args} ${target}
+}
