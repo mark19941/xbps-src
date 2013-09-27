8a94ddfe23646980ab3d6f2279be6fd4fac4905d
Juan RP <xtraeme@gmail.com>
2013-09-27 02:41:42 +0200

    New build_style: waf3 (waf with python3).

---
+++ build_style/waf3.sh
@@ -0,0 +1,17 @@
+#
+# This helper is for templates using WAF with python3 to build/install.
+#
+do_configure() {
+	PYTHON=python3 python3 waf configure --prefix=/usr ${configure_args}
+}
+
+do_build() {
+	PYTHON=python3 python3 waf build ${make_build_args}
+}
+
+do_install() {
+	if [ -z "$make_install_args" ]; then
+		make_install_args="--destdir=$DESTDIR"
+	fi
+	PYTHON=python3 python3 waf install ${make_install_args}
+}
