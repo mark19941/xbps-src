2351976aa7819790987a5b2cd39a10a9f7760c6e
Juan RP <xtraeme@gmail.com>
2013-11-13 23:17:40 +0100

    libexec/xbps-src-dopkg.sh: fix 027cae2 and use relative path.

--- libexec/xbps-src-dopkg.sh
+++ libexec/xbps-src-dopkg.sh
@@ -46,7 +46,7 @@ process_metadata_scripts() {
 
 export PATH="/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
-TRIGGERSDIR="/usr/libexec/xbps-triggers"
+TRIGGERSDIR="./usr/libexec/xbps-triggers"
 ACTION="\$1"
 PKGNAME="\$2"
 VERSION="\$3"
