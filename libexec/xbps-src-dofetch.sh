8277b2d4fa2c87b10f6e075b2c020a63d74e8608
Juan RP <xtraeme@gmail.com>
2014-01-28 23:08:55 +0100

    libexec/xbps-src-dofetch.sh: read environment files before setup_pkg().
    Otherwise vars defined on them aren't set up when the template is read.

--- libexec/xbps-src-dofetch.sh
+++ libexec/xbps-src-dofetch.sh
@@ -28,15 +28,11 @@ PKGNAME="$1"
 
 . $XBPS_SHUTILSDIR/common.sh
 
-for f in $XBPS_COMMONDIR/helpers/*.sh; do
+for f in $XBPS_COMMONDIR/environment/*.sh $XBPS_COMMONDIR/helpers/*.sh; do
 	. $f
 done
 
 setup_pkg "$PKGNAME"
-
-for f in $XBPS_COMMONDIR/environment/*.sh; do
-	. $f
-done
 
 if [ -z $pkgname ]; then
 	exit 1
